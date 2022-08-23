e8b644c36870bbe26b6769508e7adacda6be396b
Morpheus-23 <skyelah@icloud.com>
2022-08-23 01:29:41 +0000

    changed properties for roast and grind and views.py

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+import json
 
 # Create your views here.
 
@@ -14,12 +15,31 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+
+    roast = None
+    grind = None
+    if 'product_roast' in request.POST:
+        roast = request.POST['product_roast']
+    if 'product_grind' in request.POST:
+        grind = request.POST['product_grind']
+
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if roast or grind:
+        if item_id in list(bag.keys()):
+            if bag[item_id]['bean_options']['roast'] == roast and bag[item_id]['bean_options']['grind'] == grind:
+                bag[item_id] += quantity
+            else:
+                bag[item_id] = quantity
+        else:
+                bag[item_id] = quantity
+                bag[item_id]['bean_options']['roast'] = roast
+                bag[item_id]['bean_options']['grind'] = grind
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
